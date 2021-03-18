8a6629222f9b933c841e81cf9ceed6cc0b6c15af
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2021-03-18 17:25:56 +0100

    add hymnen

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
-basic.forever(function () {
-	
-})
+let hymnen = ["R:2", "G:2", "G:2", "G:2", "F:2", "E:2", "D:2", "D:4", "C:4", "R:8", "R:2", "A:2", "A:2", "A:2", "G:2", "F:2", "E:2", "D:4", "R:8", "R:2", "G:2", "G:2", "G:2", "F:2", "E:2", "D:2", "D:4", "C#:4", "R:4", "A3:2", "A3:2", "F4:4", "F4:2", "F4:2", "F4:2", "E:2", "D:2", "C:2", "C:4"]
+
+music.setTempo(80)
+music.beginMelody(hymnen, MelodyOptions.Once)
+
